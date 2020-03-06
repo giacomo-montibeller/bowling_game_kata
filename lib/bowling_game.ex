ca4a0253729479a10fa46b727ef1d7e603302657
Giacomo Montibeller <giacomo.montibeller@xpeppers.com>
2020-03-06 17:35:13 +0100

    add test for simple frame (no spare nor strike)

--- lib/bowling_game.ex
+++ lib/bowling_game.ex
@@ -2,4 +2,8 @@ defmodule BowlingGame do
   def score() do
     0
   end
+
+  def score([first, second]) do
+    first + second
+  end
 end
--- test/bowling_game_test.exs
+++ test/bowling_game_test.exs
@@ -5,5 +5,9 @@ defmodule BowlingGameTest do
     test "for empty game will return 0" do
       assert BowlingGame.score() == 0
     end
+
+    test "for single frame with roll 1,2 (no spare or strike) will return 3" do
+      assert BowlingGame.score([1, 2]) == 3
+    end
   end
 end
