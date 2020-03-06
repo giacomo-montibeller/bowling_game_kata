e60d453546c8e12fb89fd9b93ce88316c1f914ee
Giacomo Montibeller <giacomo.montibeller@xpeppers.com>
2020-03-06 17:27:03 +0100

    initial commit

---
+++ lib/bowling_game_kata.ex
@@ -0,0 +1,18 @@
+defmodule BowlingGameKata do
+  @moduledoc """
+  Documentation for `BowlingGameKata`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BowlingGameKata.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bowling_game_kata_test.exs
@@ -0,0 +1,8 @@
+defmodule BowlingGameKataTest do
+  use ExUnit.Case
+  doctest BowlingGameKata
+
+  test "greets the world" do
+    assert BowlingGameKata.hello() == :world
+  end
+end
